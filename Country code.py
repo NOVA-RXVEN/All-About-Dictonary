82f60589db1c4e062bcce789a97cb86b97aef025
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-02-21 18:39:40 +0600

    first commit

---
+++ Country code.py
@@ -0,0 +1,14 @@
+country_code = {
+    "India" : "+91",
+    "USA" : "+1",
+    "Bangladesh" : "+880"
+}
+
+print("Country code for India - ")
+print(country_code.get("India", "Not Found"))
+
+print("Country code for USA - ")
+print(country_code.get("Japan", "Not Found"))
+
+print("Country code for India - ")
+print(country_code.get("Bangladesh", "Not Found"))
